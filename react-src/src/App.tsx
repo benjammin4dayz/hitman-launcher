9fc179fbb7e6b1369a7246381e46498e89424368
Jam <42326027+benjammin4dayz@users.noreply.github.com>
2024-12-23 02:23:42 -0500

    chore: setup chakra ui

--- react-src/src/App.tsx
+++ react-src/src/App.tsx
@@ -1,13 +1,17 @@
-import { useNeutralinoContext } from './NeutralinoProvider';
+import { useNeutralinoContext } from '@/NeutralinoProvider';
+import { Box, Heading, VStack } from '@chakra-ui/react';
+import { Button } from './components/ui/button';
 
 function App() {
   const { exit } = useNeutralinoContext();
 
   return (
-    <div>
-      <p>Hitman Launcher</p>
-      <button onClick={exit}>Exit</button>
-    </div>
+    <Box h="100vh" display="flex" justifyContent="center" alignItems="center">
+      <VStack>
+        <Heading as="h1">Hitman Launcher</Heading>
+        <Button onClick={exit}>Exit</Button>
+      </VStack>
+    </Box>
   );
 }
 
--- react-src/src/main.tsx
+++ react-src/src/main.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
-import NeutralinoProvider from './NeutralinoProvider';
+import App from '@/App';
+import NeutralinoProvider from '@/NeutralinoProvider';
+import { Provider as ChakraProvider } from '@/components/ui/provider';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <NeutralinoProvider>
-      <App />
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
     </NeutralinoProvider>
   </React.StrictMode>
 );
