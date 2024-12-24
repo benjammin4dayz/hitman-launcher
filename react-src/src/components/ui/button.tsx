c1d53806909f501c2e6acbe4399a4e2054eb3f09
Jam <42326027+benjammin4dayz@users.noreply.github.com>
2024-12-24 00:36:18 -0500

    chore: fix build error

--- react-src/src/components/ui/button.tsx
+++ react-src/src/components/ui/button.tsx
@@ -6,7 +6,7 @@ import {
   Spinner,
 } from '@chakra-ui/react';
 import * as React from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 interface ButtonLoadingProps {
   loading?: boolean;
