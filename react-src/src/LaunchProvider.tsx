de191ad53e7a8a42ef1de3bcca17c0a5d69bdd52
Jam <42326027+benjammin4dayz@users.noreply.github.com>
2024-12-25 01:04:52 -0500

    chore: temp fix unhandled error in spawn command
    The underlying issue might be related to 45ceda8c; sometimes the value
    may be a string and other times it may be an object/array like
    `['value']`.

--- react-src/src/LaunchProvider.tsx
+++ react-src/src/LaunchProvider.tsx
@@ -216,17 +216,24 @@ async function spawnNeutralinoManagedProcess({
     processPath: path,
     processName: name,
     onStdErr: evt => {
+      // TODO: fix this type
       if (
-        typeof evt === 'string' &&
-        evt.includes('is not recognized as an internal or external')
+        (typeof evt === 'string' &&
+          evt.includes('is not recognized as an internal or external')) ||
+        (evt instanceof Array &&
+          (evt[0] as string)?.includes(
+            'is not recognized as an internal or external'
+          ))
       ) {
         onError?.(
           `${name.charAt(0).toUpperCase() + name.slice(1)} failed to launch`
         );
         dispatchStopAction();
       } else if (
-        evt instanceof Array &&
-        (evt[0] as string)?.includes('The system cannot find the')
+        (typeof evt === 'string' &&
+          evt.includes('The system cannot find the')) ||
+        (evt instanceof Array &&
+          (evt[0] as string)?.includes('The system cannot find the'))
       ) {
         onError?.(`Invalid ${name} path`);
         dispatchStopAction();
