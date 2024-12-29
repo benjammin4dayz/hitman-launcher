44994481b15afa621c61f5825a8b82a613a91d07
Jam <42326027+benjammin4dayz@users.noreply.github.com>
2024-12-29 09:50:02 -0500

    chore: ignore linter errors

--- app/src/components/ui/select.tsx
+++ app/src/components/ui/select.tsx
@@ -1,3 +1,6 @@
+/* eslint-disable @typescript-eslint/unbound-method */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 'use client';
 
 import type { CollectionItem } from '@chakra-ui/react';
